48abe72b080363fba512c33db01c6b9aca2c1050
SUBHRAAAAA <150889119+SUBHRAAAAA@users.noreply.github.com>
2025-10-02 22:08:50 +0530

    Create app.routes.ts

---
+++ app.routes.ts
@@ -0,0 +1,22 @@
+import { MissionComponent } from './../pages/mission/mission.component';
+import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { LoginComponent } from '../pages/login/login.component';
+import { HomeComponent } from '../pages/home/home.component';
+
+export const routes: Routes = [
+  
+    {
+        path:'',
+        component:HomeComponent
+    },
+    {
+        path:'login',
+        component:LoginComponent 
+    },
+    {
+        path:'mission',
+        component:MissionComponent 
+    }
+
+];
